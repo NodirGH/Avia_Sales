54e1c12e0adff0b0bb0bc40ce034c024e88d4262
nodirbekmahkamov <mahkamov.nodirbek.95@gmail.com>
2024-04-26 11:10:15 +0500

    Fragments added. Bottom navigation is set, navigation component is installed

--- app/src/main/java/com/example/aviasales/MainActivity.kt
+++ app/src/main/java/com/example/aviasales/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.aviasales
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.aviasales.databinding.ActivityMainBinding
+import extension.hideAnimWithSlideDown
+import extension.showAnimWithSlideUp
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
+        binding.bottomNavigationView.itemIconTintList = null
+        navController = navHostFragment.navController
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment,
+                R.id.hotelsFragment,
+                R.id.inShortsFragment,
+                R.id.subscribeFragment,
+                R.id.profileFragment -> {
+                    binding.bottomNavigationView.showAnimWithSlideUp()
+                }
+                else -> {
+                    binding.bottomNavigationView.hideAnimWithSlideDown()
+                }
+            }
+        }
     }
 }
